d3504248ce0985233355e9509d54c1306b548b91
Jihyunidaaa <jihyunidaaa@gmail.com>
2023-10-25 19:41:08 +0000

    Dynamic Array Basic Implementation + Unit tests

--- src/main/java/edu/penn/jhkim/DynamicArray.java
+++ src/main/java/edu/penn/jhkim/DynamicArray.java
@@ -1,4 +1,75 @@
 package edu.penn.jhkim;
 
-public class DynamicArray {
+public class DynamicArray<T> {
+    private Object[] data;
+    private int size;
+    private int capacity;
+
+    public DynamicArray(){
+        this(1);
+    }
+
+    public DynamicArray(int initialCapacity) {
+        data = new Object[initialCapacity];
+        size = 0;
+        capacity = initialCapacity;
+    }
+
+    //Complexity: O(1) - Amortized
+    public void add(T newElement){
+        if(size == capacity){
+            resize();
+        }
+        data[size++] = newElement;
+    }
+
+    //Complexity: O(n)
+    public void add(int index, T newElement){
+        if(index > size){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if(size == capacity){
+            resize();
+        }
+
+        for(int i = size - 1; i >= index; i--){
+            data[i+1]= data[i];
+        }
+        data[index] = newElement;
+        size++;
+    }
+
+    //Complexity: O(1)
+    public T get(int index){
+        if(index > size){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (T) data[index];
+    }
+
+    //Complexity: O(1)
+    public int size(){
+        return size;
+    }
+
+    //Complexity: O(n)
+    public void remove(int index){
+        if(index > size){
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        for(int i = index; i < size - 1; i++){
+            data[i] = data[i + 1];
+        }
+        size--;
+    }
+
+    private void resize(){
+        Object[] tmp = data;
+        capacity *= 2;
+        data = new Object[capacity];
+
+        for(int i = 0; i < size; i++){
+            data[i] = tmp[i];
+        }
+    }
 }
--- src/test/java/edu/penn/jhkim/DynamicArrayTest.java
+++ src/test/java/edu/penn/jhkim/DynamicArrayTest.java
@@ -1,4 +1,120 @@
 package edu.penn.jhkim;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
 public class DynamicArrayTest {
+    private DynamicArray<Integer> array;
+    private ArrayList<Integer> ans;
+    Random random;
+
+    @Before
+    public void init(){
+        array = new DynamicArray<>();
+        ans = new ArrayList<>();
+        random = new Random();
+    }
+
+    @After
+    public void tearDown(){
+        array = null;
+        ans = null;
+        random = null;
+    }
+
+    private void verify(int size){
+        assertEquals(size, array.size());
+        assertEquals(size, ans.size());
+
+        for(int i = 0; i < size; i++){
+            assertEquals(ans.get(i), array.get(i));
+        }
+    }
+
+    @Test
+    public void addTest(){
+        final int ADD_ITERS = 10;
+
+        for(int i = 0; i < ADD_ITERS; i++){
+            array.add(i);
+            ans.add(i);
+        }
+
+        verify(ADD_ITERS);
+    }
+
+    @Test
+    public void insertFrontTest(){
+        final int INSERT_ITERS = 10;
+
+        for(int i = INSERT_ITERS; i > 0; i--){
+            array.add(0, i);
+            ans.add(0, i);
+        }
+
+        verify(INSERT_ITERS);
+    }
+
+    @Test
+    public void insertRandomTest(){
+        final int INSERT_ITERS = 10;
+
+        array.add(0);
+        ans.add(0);
+
+        for(int i = 1; i < INSERT_ITERS; i++){
+            int randomIndex = random.nextInt(array.size());
+            array.add(randomIndex, i);
+            ans.add(randomIndex, i);
+        }
+
+        verify(INSERT_ITERS);
+    }
+
+    @Test
+    public void removeFrontTest(){
+        final int REMOVE_ITERS = 10;
+        final int MAX_INT = 10000;
+
+        for(int i = 0; i < REMOVE_ITERS; i++){
+            int nextRandomInt = random.nextInt(MAX_INT);
+            array.add(nextRandomInt);
+            ans.add(nextRandomInt);
+        }
+
+        verify(REMOVE_ITERS);
+
+        for(int i = 0; i < REMOVE_ITERS; i++){
+            array.remove(0);
+            ans.remove(0);
+            verify(REMOVE_ITERS - i - 1);
+        }
+    }
+
+    @Test
+    public void removeRandomTest(){
+        final int REMOVE_ITERS = 10;
+        final int MAX_INT = 10000;
+
+        for(int i = 0; i < REMOVE_ITERS; i++){
+            int nextRandomInt = random.nextInt(MAX_INT);
+            array.add(nextRandomInt);
+            ans.add(nextRandomInt);
+        }
+
+        verify(REMOVE_ITERS);
+
+        for(int i = 0; i < REMOVE_ITERS; i++){
+            int randomIndex = random.nextInt(REMOVE_ITERS - i);
+            array.remove(randomIndex);
+            ans.remove(randomIndex);
+            verify(REMOVE_ITERS - i - 1);
+        }
+    }
 }
